1cdac173a0997d0f3a93919a7ce2d07b348e59be
Jer Carlo Catallo <49897728+rojerthat07@users.noreply.github.com>
2019-12-24 19:14:31 +0800

    comments

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,14 @@ const gulp = require('gulp');
 const uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 
-
+//Gulp Uglify
 gulp.task('scripts', function() {
   return gulp.src('src/scripts/app.js')
     .pipe(concat('app.js'))
     .pipe(gulp.dest('build/scripts'));
 });
 
-
+//Gulp Concat
 gulp.task('compress', () => {
           return gulp.src('src/scripts/particles.js')
           .pipe(uglify())
